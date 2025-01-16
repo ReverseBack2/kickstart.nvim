656007899644f35e18dc41b2a32c62ab75451966
ReverseBack <donovan.burk@gmail.com>
2025-01-15 22:19:24 -0800

    added colors

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -355,6 +355,10 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'scottmckendry/cyberdream.nvim', lazy = false, priority = 1000 },
+      { 'maxmx03/fluoromachine.nvim', lazy = false, priority = 1000 },
+      { 'stankovictab/mgz.nvim', lazy = false, priority = 1000 },
+      { 'hachy/eva01.vim', lazy = false, priority = 1000 },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -954,3 +958,7 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- These are my Changes
+vim.cmd.colorscheme 'mgz'
+vim.opt.termguicolors = true
